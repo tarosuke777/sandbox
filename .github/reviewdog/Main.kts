f822a54857974de675b16469ecf562f7ca8346dc
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-09-27 17:37:34 +0900

    'test'

---
+++ .github/reviewdog/Main.kts
@@ -0,0 +1,23 @@
+import java.io.File
+
+fun main() {
+    val targetDirectory = File("./test")
+    val report = File("./result.txt")
+    report.createNewFile()
+    val keyword = "TODO"
+
+    targetDirectory
+        .walk() // フォルダ内の全ファイルとディレクトリを走査します
+        .filter { it.isFile } // ファイルだけを対象とします
+        .forEach { file ->
+            // ファイルの各行を走査します
+            file.readLines().forEachIndexed { index, line ->
+                // 特定のキーワードが含まれている行を出力します
+                if (line.contains(keyword)) {
+                    report.appendText("${file.path}:${index + 1}:1: TODO found \n")
+                }
+            }
+        }
+}
+
+main()
