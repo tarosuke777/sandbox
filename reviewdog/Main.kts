175f474a6e27a4220f1da0ea507c23ba64b755e7
tarosuke <37805199+tarosuke777@users.noreply.github.com>
2025-10-05 11:35:53 +0900

    Update Main.kts

--- reviewdog/Main.kts
+++ reviewdog/Main.kts
@@ -14,7 +14,7 @@ fun main() {
             file.readLines().forEachIndexed { index, line ->
                 // 特定のキーワードが含まれている行を出力します
                 if (line.contains(keyword)) {
-                    report.appendText("${file.path}:${index + 1}:1:w: :warning: :x: :information_source: TODO found \ntwo line\nthree line\n")
+                    report.appendText("${file.path}:${index + 1}:1:w: > [!CAUTION] \n > test.\n")
                 }
             }
         }
